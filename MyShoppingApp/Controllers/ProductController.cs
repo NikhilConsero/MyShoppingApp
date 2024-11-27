1b22644ae052d9304fbf82b3a61e0a69c6caa448
NikhilConsero <nikhil.gurudev@conseroglobal.com>
2024-11-27 13:38:46 +0530

    ch5

--- MyShoppingApp/Controllers/ProductController.cs
+++ MyShoppingApp/Controllers/ProductController.cs
@@ -8,17 +8,17 @@ namespace MyShoppingApp.Controllers
         public static List<Product> productList = new List<Product>();
         public IActionResult Index()
         {
-            List<int> products = new List<int>();
-            products.Add(1);
-            List<Product> productlist = new List<Product>();
-            Product productobj = new Product();
-            productobj.Id = 1;
-            productobj.Name = "Test1";
-            productobj.Description = "Test1";
-            productobj.Category = "Sports";
-            productlist.Add(productobj);
+            //List<int> products = new List<int>();
+            //products.Add(1);
+            //List<Product> productlist = new List<Product>();
+            //Product productobj = new Product();
+            ////productobj.Id = 1;
+            //productobj.Name = "Test1";
+            //productobj.Description = "Test1";
+            //productobj.Category = "Sports";
+            //productlist.Add(productobj);
 
-            return View(productlist);
+            return View(productList);
         }
 
         public IActionResult AddProduct()
@@ -30,12 +30,21 @@ namespace MyShoppingApp.Controllers
         {
             //How to pass data from action to view page
             ViewBag.message = "Successfully Added......!";
+            if (productList.Count==0)
+            {
+                product.Id = 1;
+            }
+            else
+            {
+                product.Id = productList[productList.Count - 1].Id + 1;
+            }
             productList.Add(product);
             return View("index",productList);
         }
         public IActionResult EditProduct(int id)
         {
-            return View();
+            var result = productList.Where(obj=>obj.Id == id).ToList();
+            return View(result[0]);
         }
     }
 }
